9ef4f023055dddb61908ee687a04043c1ef829c6
chibuikeeugene <chibuikeeugene@gmail.com>
2024-08-25 16:27:18 +0100

    updated the main module

--- q_and_a/q_and_a/chat_prompt_template.py
+++ q_and_a/q_and_a/chat_prompt_template.py
@@ -11,11 +11,33 @@ from langchain_core.prompts import MessagesPlaceholder
 
 from langchain.chains import create_retrieval_chain
 
-def prompt_template(llm_model, vector_object):
-    """ customize the template prompt"""
+def retrieval_chain(llm_model, vector_object):
+    """ customize the template prompt and retriever chain"""
+    prompt = ChatPromptTemplate.from_template(
+        """Answer the following question based only on the provided context, assuming the role of a customer support engineer:
+            <context>
+            {context}
+            </context>                     
+            Question: {input}"""
+)
+
+    # create a document chain that holds user input prompt and the model
+    document_chain = create_stuff_documents_chain(prompt=prompt, llm=llm_model)
+
+    # creating a retriever object from the vectorstore created in the llmmodel module
+    retriever = vector_object.as_retriever()
+
+    # creating our retiever chain
+    retrieval_chain_object = create_retrieval_chain(retriever, document_chain)
+
+    return retrieval_chain_object
+
+
+def retrieval_chain_with_history(llm_model, vector_object):
+    """ customize the template prompt and retriever chain with history chats"""
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "you are a renowned customer support engineer. Answer the user's questions based on the below context:\n\n{context}"),
+            ("system", "Answer the user's questions based on the below context:\n\n{context}"),
             MessagesPlaceholder(variable_name="chat_history"),
             ("user", "{input}"),
         ]
@@ -23,7 +45,6 @@ def prompt_template(llm_model, vector_object):
 
     # create a document chain that holds user input prompt and the model
     document_chain = create_stuff_documents_chain(prompt=prompt, llm=llm_model)
-    
 
     # creating a retriever object from the vectorstore created in the llmmodel module
     retriever = vector_object.as_retriever()
@@ -32,6 +53,6 @@ def prompt_template(llm_model, vector_object):
     retriever_chain_with_history = create_history_aware_retriever(llm =llm_model, prompt=prompt, retriever=retriever)
 
     # creating our retiever chain
-    retrieval_chain_update = create_retrieval_chain(retriever_chain_with_history, document_chain)
+    retrieval_chain_with_history_object = create_retrieval_chain(retriever_chain_with_history, document_chain)
 
-    return retrieval_chain_update
+    return retrieval_chain_with_history_object
--- q_and_a/q_and_a/main.py
+++ q_and_a/q_and_a/main.py
@@ -1,80 +1,126 @@
-import fire
+import os
+from pathlib import Path
+parent_dir = Path(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+faiss_vectorstore_dir = Path(os.path.join(parent_dir, 'faiss_vectorstore_object'))
+
+
 import streamlit as st
 import llm_model
 import chat_prompt_template
 from loguru import logger
+from typing import Optional, Any
 
 from langchain_core.messages import AIMessage, HumanMessage
+import joblib
 
 from dotenv import load_dotenv
 load_dotenv()
 
 from document_loader import web_document_loader
 
-# define global variables
-chat_history:list = []
 
-def langsmith_app(prompt:str, site_link):
+st.set_page_config(page_title="Q&A with any website")
 
-    # retrieve url of any website
-    # site_link =  input("Enter the URL of the website you want to index: ")
+st.header("LangChain Application")
 
-    # call the web loader module
-    logger.info('loading the website document')
-    docs = web_document_loader(url = site_link)
+input=st.text_input("Enter the URL of the website you want to index: ", key="input")
 
-    # call the llm model module
-    logger.info('loading the LLM model')
-    llm = llm_model.load_model(model_name = "llama3")
+question = st.text_input("Ask a question about the website: ")
 
-    # load the embedding layer module
-    logger.info('generating the embeddings for the website contents...')
-    vector  = llm_model.generate_embeddings(model_name="llama3", loaded_document=docs)
+# display the website in focus to the user
+st.subheader(f"{input}")
 
-    # call the chat prompt template module
-    logger.info('creating the retrieval chain')
-    retrieval_chain_with_history = chat_prompt_template.prompt_template(
-        llm_model=llm,
-        vector_object=vector
-    )
-    logger.info('Response is being retrieved!')
-    response =  retrieval_chain_with_history.invoke(
-        {
-            "chat_history": chat_history,
-            "input": prompt
-        }
+submit=st.button("Enter")
+
+
+def langsmith_app(prompt:str, site_link:str,):
+    " a question and answer function when given a website and a query prompt"
+    if site_link and prompt:
+
+        # call the web loader module
+        logger.info('loading the website document')
+        docs = web_document_loader(url = site_link)
+
+        # call the llm model module
+        logger.info('loading the LLM model')
+        llm = llm_model.load_model(model_name = "llama3")
+
+        # load the embedding layer module
+        logger.info('generating the embeddings for the website contents...')
+        vector  = llm_model.generate_embeddings(model_name="llama3", loaded_document=docs)
+
+        logger.info('embeddings generated and saved!')
+        joblib.dump(vector, f"{faiss_vectorstore_dir}/vectorstore.pkl")
+
+        # call the chat prompt template module
+        logger.info('creating the retrieval chain')
+        retrieval_chain = chat_prompt_template.retrieval_chain(
+            llm_model=llm,
+            vector_object=vector
         )
-    return response['answer']
+        logger.info('Response is being retrieved!')
+        response =  retrieval_chain.invoke(
+            {
+                "input": prompt
+            }
+            )
+        return response['answer']
+
+
+
+def langsmith_app_with_chat_history(prompt:str, chat_history:Optional[list | None], contexts:str, vector_object):
+    """ a method to provide continuous response based on your conversation history"""
+    if prompt:
+        # call the llm model module
+        logger.info('loading the LLM model')
+        llm = llm_model.load_model(model_name = "llama3")
+
+        # call the chat prompt template module
+        logger.info('creating the retrieval chain')
+        retrieval_chain_with_history = chat_prompt_template.retrieval_chain_with_history(
+            llm_model=llm,
+            vector_object=vector_object
+        )
+        logger.info('Response is being retrieved!')
+        response =  retrieval_chain_with_history.invoke(
+            {
+                "chat_history": chat_history,
+                "input": prompt,
+                "context": contexts
+            }
+            )
+        return response['answer']
+
+
 
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Q&A with any website")
 
-    st.header("LangChain Application")
+    # if user enters a valid website and a prompt
+    if input:
+        if submit:
+            response = langsmith_app(prompt=question, site_link=input,)
+            st.subheader("The Response is")
+            st.write(response)
+        # previous_question =  question
 
-    input=st.text_input("Enter the URL of the website you want to index: ", key="input")
-    
-    question = st.text_input("Ask a question about the website: ")
-
-    # display the website in focus to the user
-    st.subheader(f"{input}")
-
-    submit=st.button("Enter")
-
-    if submit:
-        response = langsmith_app(prompt=question, site_link=input)
-        st.subheader("The Response is")
-        st.write(response)
-        chat_history = [
-            HumanMessage(
-                content= f"{question}"
-            ), 
-            AIMessage(
-                content= f"{response}"
-            )
-        ]
+    # # if user enters only a query prompt
+    # if question:
+    #     if submit:
+    #         vector_obj = joblib.load(f"{faiss_vectorstore_dir}/vectorstore.pkl")
+    #         context = "Site related queries."
+    #         chats_history = [
+    #             HumanMessage(
+    #                 content= f"{previous_question}"
+    #             ),
+    #         ]
+    #         new_response = langsmith_app_with_chat_history(
+    #             prompt=question,  
+    #             chat_history=chats_history, 
+    #             vector_object= vector_obj,
+    #             contexts= context)
+    #         st.subheader("The Response is")
+    #         st.write(new_response)
+        
             
-            
-
-    
 
